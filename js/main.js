b23f6fa5d678894caf9b742c0ec02774eee9ea9d
Sammy Teahan <sam.teahan@aggiemail.usu.edu>
2015-07-03 15:28:12 -0600

    Finished marketing site

--- js/main.js
+++ js/main.js
@@ -1,3 +1,35 @@
 (function() {
-  console.log('script loaded');
+
+  /**
+  * Initialize smooth scroll
+  */
+  smoothScroll.init({
+    speed: 1000,
+    easing: 'easeInOutCubic',
+    updateURL: false,
+    offset: 0
+  });
+
+  /**
+  * Allow user to change src of images in armor section
+  */
+  var mainImg = document.querySelector('.armor img');
+  var thumbOne = document.querySelector('.thumb1');
+  var thumbTwo = document.querySelector('.thumb2');
+  var thumbThree = document.querySelector('.thumb3');
+  var thumbFour = document.querySelector('.thumb4');
+  
+  /**
+  * Event Handlers
+  */
+
+  var setAsMain = function() {
+    mainImg.src = this.src;
+  }
+
+  thumbOne.addEventListener('click', setAsMain);
+  thumbTwo.addEventListener('click', setAsMain);
+  thumbThree.addEventListener('click', setAsMain);
+  thumbFour.addEventListener('click', setAsMain);
+
 }());
